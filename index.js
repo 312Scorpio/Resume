5992a02c42034f3690dcf6ab0ae7ff73544fa45a
Orlando Sirbas <312Scorpio@gmail.com>
2025-07-26 04:02:32 -0500

    audio test

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-var randomNumber = Math.floor(Math.random() * 3) + 1; //1-3
+// generate random number from 1-3
+var randomNumber = Math.floor(Math.random() * 3) + 1;
 
-var audio = document.getElementsByName("audio")[0];
-
-if (randomNumber == 2)
+// play 1of3 audio tracks
+if (randomNumber === 1)
 {
-    audio.setAttribute("src") = "./assets/sounds/what must be done.mp3";
+    var audio = new Audio("./assets/sounds/mercy.mp3");
 }
-else if (randomNumber == 3)
+else if (randomNumber === 2)
 {
-    audio.setAttribute("src") = "./assets/sounds/conscience.mp3";
+    var audio = new Audio("./assets/sounds/what must be done.mp3");
 }
+else
+{
+    var audio = new Audio("./assets/sounds/conscience.mp3");
+}
+audio.play();
